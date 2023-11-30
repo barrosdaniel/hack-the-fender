1be43c8ffbf71e1963bed1e3f0b6a3124daa4e99
Daniel Barros <daniel.barros@cqumail.com>
2023-11-30 22:31:35 +0800

    Complete Notifying the Boss

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 compromised_users = []
 
@@ -13,3 +14,11 @@ print(compromised_users)
 with open('compromised_users.txt', 'w') as compromised_user_file:
     for username in compromised_users:
         compromised_user_file.write(f"\n{username}")
+
+with open('boss_message.json', 'w') as boss_message:
+    boss_message_dict = {
+        'recipient': 'The Boss',
+        'message': 'Mission Success'
+    }
+    json.dump(boss_message_dict, boss_message)
+    
