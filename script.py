14287731a18a34df29a5bbac5a6ad24634548ace
Daniel Barros <daniel.barros@cqumail.com>
2023-11-30 22:25:58 +0800

    Complete Reading In the Passwords.

--- script.py
+++ script.py
@@ -1,1 +1,15 @@
-print('Hello, Python!')
+import csv
+
+compromised_users = []
+
+with open('passwords.csv') as password_file:
+    password_csv = csv.DictReader(password_file)
+    for row in password_csv:
+        password_row = row
+        compromised_users.append(password_row['Username'])
+
+print(compromised_users)
+
+with open('compromised_users.txt', 'w') as compromised_user_file:
+    for username in compromised_users:
+        compromised_user_file.write(f"\n{username}")
